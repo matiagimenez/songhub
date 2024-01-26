8894c8f6767454f0be0d8b93fa751e005ea407da
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-01-26 12:05:55 -0300

    mejoras en la accesibilidad del menu

--- scripts/animations/menu-animation.js
+++ scripts/animations/menu-animation.js
@@ -2,12 +2,29 @@ const menuButton = document.querySelector('.menu-button');
 const menuButtonContent = document.querySelector('.menu-button span');
 const nav = document.querySelector('.header-nav ul');
 
+window.addEventListener('resize', () => {
+	if (window.innerWidth < 900) {
+		nav.classList.add('hidden');
+		menuButton.setAttribute('aria-hidden', 'false');
+		menuButton.setAttribute('aria-expanded', 'false');
+	} else {
+		menuButton.setAttribute('aria-hidden', 'true');
+		nav.setAttribute('aria-hidden', 'false');
+	}
+});
+
 document.addEventListener('click', (event) => {
 	if (event.target === menuButton || event.target === menuButtonContent) {
 		nav.classList.toggle('hidden');
 
-		const ariaCurrentValue = menuButton.getAttribute('aria-expanded');
-		menuButton.setAttribute('aria-expanded', !JSON.parse(ariaCurrentValue));
+		const ariaExpandedCurrentValue =
+			menuButton.getAttribute('aria-expanded');
+		const ariaHiddenCurrentValue = nav.getAttribute('aria-hidden');
+		menuButton.setAttribute(
+			'aria-expanded',
+			!JSON.parse(ariaExpandedCurrentValue)
+		);
+		nav.setAttribute('aria-hidden', !JSON.parse(ariaHiddenCurrentValue));
 
 		const firstItem = document.querySelector(
 			'.header-nav ul li:first-child'
@@ -19,5 +36,7 @@ document.addEventListener('click', (event) => {
 document.addEventListener('keyup', (event) => {
 	if (event.key === 'Escape') {
 		nav.classList.add('hidden');
+		nav.setAttribute('aria-hidden', 'true');
+		menuButton.setAttribute('aria-expanded', 'false');
 	}
 });
