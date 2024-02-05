e3f8b39b39ce935ff5a696a5f9ed2e711bef0bf4
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-02-05 08:59:55 -0300

    add background opacity when menu is open

--- scripts/animations/menu-animation.js
+++ scripts/animations/menu-animation.js
@@ -2,15 +2,16 @@ const menuButton = document.querySelector('.menu-button');
 const menuButtonContent = document.querySelector('.menu-button span');
 const menu = document.querySelector('.header-nav ul');
 const body = document.body;
+const main = document.querySelector('main');
 
 function updateMenuVisibility() {
 	if (window.innerWidth < 900) {
-		menu.classList.add('hidden')
-		body.classList.remove('none-scroll')
-		menu.classList.remove('view-menu')
-		menu.classList.remove('close-menu')
-		menuButton.classList.remove('close-menu-button')
-		menuButton.classList.add('open-menu-button')
+		menu.classList.add('hidden');
+		body.classList.remove('none-scroll');
+		menu.classList.remove('view-menu');
+		menu.classList.remove('close-menu');
+		menuButton.classList.remove('close-menu-button');
+		menuButton.classList.add('open-menu-button');
 		menuButton.setAttribute('aria-hidden', 'false');
 		menuButton.setAttribute('aria-expanded', 'false');
 		menu.setAttribute('aria-hidden', 'true');
@@ -19,6 +20,7 @@ function updateMenuVisibility() {
 		menuButton.setAttribute('aria-hidden', 'true');
 		menuButton.setAttribute('aria-expanded', 'true');
 		menu.setAttribute('aria-hidden', 'false');
+		main.style.opacity = 1;
 	}
 }
 
@@ -31,24 +33,26 @@ window.onload = function () {
 document.addEventListener('click', (event) => {
 	if (event.target === menuButton || event.target === menuButtonContent) {
 		if (menu.classList.contains('hidden')) {
-			body.classList.add('none-scroll')
-			menu.classList.remove('close-menu')
-			menu.classList.remove('hidden')
-			menu.classList.add('view-menu')
-			menuButton.classList.remove('open-menu-button')
-			menuButton.classList.add('close-menu-button')
+			body.classList.add('none-scroll');
+			menu.classList.remove('close-menu');
+			menu.classList.remove('hidden');
+			menu.classList.add('view-menu');
+			menuButton.classList.remove('open-menu-button');
+			menuButton.classList.add('close-menu-button');
+			main.style.opacity = 0.3;
 		} else {
-			body.classList.remove('none-scroll')
-			menu.classList.remove('view-menu')
-			menu.classList.add('close-menu')
-			menuButton.classList.remove('close-menu-button')
-			menuButton.classList.add('open-menu-button')
+			body.classList.remove('none-scroll');
+			menu.classList.remove('view-menu');
+			menu.classList.add('close-menu');
+			menuButton.classList.remove('close-menu-button');
+			menuButton.classList.add('open-menu-button');
 			setTimeout(function () {
-				menu.classList.add('hidden')
+				menu.classList.add('hidden');
 			}, 400);
+			main.style.opacity = 1;
 		}
 
-		/*const ariaExpandedCurrentValue =
+		const ariaExpandedCurrentValue =
 			menuButton.getAttribute('aria-expanded');
 		const ariaHiddenCurrentValue = menu.getAttribute('aria-hidden');
 		menuButton.setAttribute(
@@ -60,18 +64,18 @@ document.addEventListener('click', (event) => {
 		const firstItem = document.querySelector(
 			'.header-nav ul li:first-child'
 		);
-		firstItem.focus();*/
+		firstItem.focus();
 	}
 });
 
 document.addEventListener('keyup', (event) => {
 	if (event.key === 'Escape') {
-		menu.classList.remove('view-menu')
-		menu.classList.add('close-menu')
-		menuButton.classList.remove('close-menu-button')
-		menuButton.classList.add('open-menu-button')
+		menu.classList.remove('view-menu');
+		menu.classList.add('close-menu');
+		menuButton.classList.remove('close-menu-button');
+		menuButton.classList.add('open-menu-button');
 		setTimeout(function () {
-			menu.classList.add('hidden')
+			menu.classList.add('hidden');
 		}, 400);
 		menu.setAttribute('aria-hidden', 'true');
 		menuButton.setAttribute('aria-expanded', 'false');
