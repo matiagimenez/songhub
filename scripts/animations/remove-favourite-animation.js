988ed421248ece36c3686de08b1fe7b19b00e0b1
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-02-14 11:00:05 -0300

    agrega remove-favourite animation en mobile

--- scripts/animations/remove-favourite-animation.js
+++ scripts/animations/remove-favourite-animation.js
@@ -2,12 +2,21 @@ const favouriteSongs = document.querySelectorAll(
 	'.edit-favourites section figure'
 );
 
-favouriteSongs.forEach((song) => {
-	song.addEventListener('mouseenter', () => {
-		song.classList.add('remove-favourite');
-	});
+window.addEventListener('resize', () => {
+	if (window.innerWidth < 900) {
+		favouriteSongs.forEach((song) => {
+			song.classList.add('remove-favourite');
+		});
+	} else {
+		favouriteSongs.forEach((song) => {
+			song.classList.remove('remove-favourite');
+			song.addEventListener('mouseenter', () => {
+				song.classList.add('remove-favourite');
+			});
 
-	song.addEventListener('mouseleave', () => {
-		song.classList.remove('remove-favourite');
-	});
+			song.addEventListener('mouseleave', () => {
+				song.classList.remove('remove-favourite');
+			});
+		});
+	}
 });
