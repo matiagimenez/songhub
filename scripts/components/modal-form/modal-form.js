f88ab93549ba15b82be15f372f500f019a137728
SimonDiLeoGIT <saimondiley@gmail.com>
2024-02-16 15:18:23 -0300

    Fix open modal

--- scripts/components/modal-form/modal-form.js
+++ scripts/components/modal-form/modal-form.js
@@ -1,5 +1,65 @@
 import { ElementBuilder } from "../../utils/ElementBuilder.js";
 
+const posts = document.querySelectorAll('.post');
+
+posts.forEach((post) => {
+
+  const more_container = ElementBuilder.createElement('section', '', {
+    class: "more-container"
+  })
+  const more_button = ElementBuilder.createElement('button', '...', {
+    class: "more-button"
+  })
+  more_container.appendChild(more_button)
+
+  const buttons_container = ElementBuilder.createElement('section', '', {
+    class: "hidden buttons-container"
+  })
+  const view_song = ElementBuilder.createElement('a', '', {
+    href: "/views/song.html"
+  })
+  const create_post = ElementBuilder.createElement('button', '', {
+    class: "post-form-opener"
+  })
+
+  create_post.addEventListener('click', () => {
+    main_header.classList.add("hidden");
+    document.body.classList.add("none-scroll")
+    create_modal();
+  })
+
+  buttons_container.appendChild(view_song)
+  buttons_container.appendChild(create_post)
+  const img = post.querySelector('.post-img');
+  img.appendChild(more_container);
+  img.appendChild(buttons_container);
+  hoverImgAction(img, buttons_container);
+  clickImgAction(buttons_container, more_button);
+});
+
+function clickImgAction(buttons_container, more_button) {
+  more_button.addEventListener('click', () => {
+    console.log(window.innerWidth)
+    if (window.innerWidth < 1000) {
+      buttons_container.classList.contains('hidden') ? buttons_container.classList.remove('hidden') : buttons_container.classList.add('hidden');
+    }
+  })
+}
+
+function hoverImgAction(img, buttons_container) {
+  img.addEventListener('mouseenter', () => {
+    if (window.innerWidth >= 1000) {
+      buttons_container.classList.remove('hidden');
+    }
+  });
+  img.addEventListener('mouseleave', () => {
+    if (window.innerWidth >= 1000) {
+      buttons_container.classList.add('hidden');
+    }
+  });
+}
+
+
 function create_modal() {
 
   const modal = ElementBuilder.createElement('section', '', {
@@ -313,8 +373,6 @@ post_form_openers.forEach((opener) => {
 
 function close_modal(modal) {
   modal.remove();
-  create_post.classList.remove("hidden");
-  // go_to_top.classList.remove("hidden");
   main_header.classList.remove("hidden");
   document.body.classList.remove("none-scroll")
 }
