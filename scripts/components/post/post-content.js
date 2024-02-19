2e058782255db894d4d2e361eccaacff04b20e87
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-02-19 09:00:30 -0300

    fix: ahora resume el contenido de todos los posts de la página

--- scripts/components/post/post-content.js
+++ scripts/components/post/post-content.js
@@ -1,12 +1,18 @@
 import { ElementBuilder } from '../../utils/ElementBuilder.js';
 
-const postContent = document.querySelector('.post-content');
+const postContent = document.querySelectorAll('.post-content');
 
-if (postContent.textContent.length > 200) {
-	postContent.textContent = postContent.textContent.slice(0, 244) + '...';
-	const postLink = ElementBuilder.createElement('a', 'Ver post completo', {
-		href: 'post.html',
-		class: 'read-more',
-	});
-	postContent.appendChild(postLink);
-}
+postContent.forEach((content) => {
+	if (content.textContent.length > 200) {
+		content.textContent = content.textContent.slice(0, 244) + '...';
+		const postLink = ElementBuilder.createElement(
+			'a',
+			'Ver post completo',
+			{
+				href: 'post.html',
+				class: 'read-more',
+			}
+		);
+		content.appendChild(postLink);
+	}
+});
