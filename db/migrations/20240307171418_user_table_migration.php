76c152ee0f224291cf0dfe689e3247913a38ce88
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-03-08 09:49:13 -0300

    fix: actualiza limites del longitud en las columnas

--- db/migrations/20240307171418_user_table_migration.php
+++ db/migrations/20240307171418_user_table_migration.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare (strict_types = 1);
 
 use Phinx\Migration\AbstractMigration;
 
@@ -9,15 +9,14 @@ final class UserTableMigration extends AbstractMigration
     public function change(): void
     {
         $table = $this->table("USER", ['id' => 'USER_ID']);
-        $table->addColumn('NAME', 'string', ['limit' => 80, 'null' => true]);
-        $table->addColumn('USERNAME', 'string', ['limit' => 50, 'null' => false]);
-        $table->addColumn('EMAIL', 'string', ['limit' => 100, 'null' => false]);
-        $table->addColumn('PASSWORD', 'string', ['limit' => 100, 'null' => false]);
+        $table->addColumn('NAME', 'string', ['limit' => 60, 'null' => true]);
+        $table->addColumn('USERNAME', 'string', ['limit' => 30, 'null' => false]);
+        $table->addColumn('EMAIL', 'string', ['limit' => 128, 'null' => false]);
+        $table->addColumn('PASSWORD', 'string', ['limit' => 255, 'null' => false]);
         $table->addColumn('SPOTIFY_ID', 'string', ['limit' => 255, 'null' => false]);
         $table->addColumn('SPOTIFY_AVATAR', 'string', ['limit' => 255, 'null' => true]);
         $table->addColumn('IS_VERIFIED', 'boolean', ['default' => false, 'null' => false]);
-        $table->addColumn('BIOGRAPHY', 'string', ['limit' => 255, 'null' => false]);
+        $table->addColumn('BIOGRAPHY', 'string', ['limit' => 160, 'null' => false]);
         $table->create();
     }
 }
-
