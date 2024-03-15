e26f218fdb758adda74328f1ebd59ada8da1e6f7
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-03-15 09:39:37 -0300

    feat: agrega columnas  a tabla USER

--- db/migrations/20240307171418_user_table_migration.php
+++ db/migrations/20240307171418_user_table_migration.php
@@ -14,6 +14,9 @@ final class UserTableMigration extends AbstractMigration
         $table->addColumn('EMAIL', 'string', ['limit' => 128, 'null' => false]);
         $table->addColumn('PASSWORD', 'string', ['limit' => 255, 'null' => false]);
         $table->addColumn('SPOTIFY_ID', 'string', ['limit' => 255, 'null' => false]);
+        $table->addColumn('ACCESS_TOKEN', 'string', ['limit' => 255, 'null' => false]);
+        $table->addColumn('ACCESS_TOKEN_GENERATION_TIME', 'timestamp', ['null' => false]);
+        $table->addColumn('REFRESH_TOKEN', 'string', ['limit' => 255, 'null' => false]);
         $table->addColumn('SPOTIFY_AVATAR', 'string', ['limit' => 255, 'null' => true]);
         $table->addColumn('IS_VERIFIED', 'boolean', ['default' => false, 'null' => false]);
         $table->addColumn('BIOGRAPHY', 'string', ['limit' => 160, 'null' => false]);
