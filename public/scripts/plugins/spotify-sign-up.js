38109ce38edb88d7d79f41fed2546a2997ce1fd5
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-03-20 19:52:01 -0300

    fix: soluciona problema de redirección

--- public/scripts/plugins/spotify-sign-up.js
+++ public/scripts/plugins/spotify-sign-up.js
@@ -2,16 +2,10 @@ const spotifyButton = document.querySelector('.sign-up-spotify');
 
 const registerWithSpotify = async () => {
 	const response = await fetch('/spotify/authorize');
-	const data = await response.json();
-	console.log(data);
+	const { ok, url } = await response.json();
 
-	// Verificar si hay una URL de redirección en el encabezado Location
-	if (response.headers.has('Location')) {
-		const redirectUrl = response.headers.get('Location');
-		window.location.href = redirectUrl; // Redirigir al usuario a la URL obtenida
-	} else {
-		const data = await response.json();
-		console.log(data); // Manejar la respuesta del servidor si no hay URL de redirección
+	if (ok) {
+		window.location.href = url; // Redirigir al usuario a la URL obtenida
 	}
 };
 
