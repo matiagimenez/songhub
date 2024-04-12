1f26d512aff3d1b6c605ee73e09e5cde019ec5e6
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-04-12 17:24:16 -0300

    refactor: actualiza algunos métodos y hace singleton

--- src/core/Session.php
+++ src/core/Session.php
@@ -2,50 +2,57 @@
 
 namespace Songhub\core;
 
+use Songhub\core\Cookie;
+
 class Session
 {
-    public function start()
-    {
-        session_start();
-    }
 
-    public function getName()
+    private static $instance;
+
+    private function __construct()
+    {}
+
+    public static function getInstance()
     {
-        return session_name();
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
     }
 
     public function destroy()
     {
-        $this->clear();
-        $sessionName = $this->getName();
-        setcookie($sessionName, '', time() - 3600);
+        session_start();
+
+        $_SESSION = [];
+        Cookie::getInstance()->delete(session_name());
+
+        ini_set('session.gc_max_lifetime', 0);
+        ini_set('session.gc_probability', 1);
+        ini_set('session.gc_divisor', 1);
+
         session_destroy();
     }
 
-    public function set($key, $value)
+    public function start($key, $value)
     {
+        if (!$this->isActive()) {
+            session_start();
+        }
+
         $_SESSION[$key] = $value;
     }
 
     public function get($key)
     {
-        return $_SESSION[$key];
-    }
-
-    public function delete($key)
-    {
-        unset($_SESSION[$key]);
+        return $_SESSION[$key] ?? null;
     }
 
     public function exists($key)
     {
         return isset($_SESSION[$key]);
     }
-    private function clear()
-    {
-        $_SESSION = [];
-    }
-
     public function isActive()
     {
         return session_status() === PHP_SESSION_ACTIVE;
