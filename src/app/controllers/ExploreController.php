c2f80a85faf5d61c6a702d930aa6d18caa4b81f5
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-04-21 12:49:14 -0300

    refactor: mejora algoritmo de recomendaciones

--- src/app/controllers/ExploreController.php
+++ src/app/controllers/ExploreController.php
@@ -10,19 +10,52 @@ use Songhub\core\HttpClient;
 class ExploreController extends Controller
 {
 
-    public function explore()
+    private $access_token = "";
+    private $seeds = null;
+    private $recommendations = null;
+
+    public function __construct()
     {
-        $access_token = Session::getInstance()->get("access_token");
+        $this->access_token = Session::getInstance()->get("access_token");
 
         //? Este array contiene información que sirve a modo de seed para solicitar las recomendaciones a Spotify API. Límite de contenido de seed = 5
-        $seeds = [
+        $this->seeds = [
             "tracks" => [],
             "artists" => [],
             "genres" => []
         ];
+    }
 
+    public function explore()
+    {
         //? Obtiene TOP de artistas escuchados por el usuario
-        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/me/top/artists", ["time_range" => "short_term", "limit" => 2], ["Authorization" => "Bearer " . $access_token]);
+        $this->getUserTops();
+
+
+        //? Valida si se pudo personalizar la recomendación en base a artistas/tracks TOP del usuario, sino recomienda en base a canciones guardadas.
+        if (!$this->validSeeds()) {
+            $this->getUserSavedTracks();
+        }
+
+        //? Valida si se pudo personalizar la recomendación, sino recomienda ultimos lanzamientos (info. más genérica).
+        if ($this->validSeeds()) {
+            //? Obtiene recomendaciones personalizadas para el usuario
+            $recommendations = $this->getRecommendations();
+
+            //TODO: Renderizar explore.view.php
+        } else {
+            //? Recomendamos nuevos lanzamientos al usuario
+            $recommendations = $this->getNewReleases();
+            //TODO: Renderizar explore.view.php
+
+        }
+        Renderer::getInstance()->explore();
+    }
+
+
+    private function getUserTops()
+    {
+        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/me/top/artists", ["time_range" => "short_term", "limit" => 2], ["Authorization" => "Bearer " . $this->access_token]);
         $body = json_decode($response["body"], true);
         $status = $response["status"];
 
@@ -35,13 +68,13 @@ class ExploreController extends Controller
             foreach ($body["items"] as $artist) {
                 $topArtistId = $artist["id"];
                 $topArtistGenre = $artist["genres"][0];
-                array_push($seeds["artists"], $topArtistId);
-                array_push($seeds["genres"], $topArtistGenre);
+                array_push($this->seeds["artists"], $topArtistId);
+                array_push($this->seeds["genres"], $topArtistGenre);
             }
         }
 
         //? Obtiene TOP de tracks escuchados por el usuario
-        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/me/top/tracks", ["time_range" => "short_term", "limit" => 1], ["Authorization" => "Bearer " . $access_token]);
+        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/me/top/tracks", ["time_range" => "short_term", "limit" => 1], ["Authorization" => "Bearer " . $this->access_token]);
         $body = json_decode($response["body"], true);
         $status = $response["status"];
 
@@ -53,70 +86,94 @@ class ExploreController extends Controller
         if (count($body["items"]) > 0) {
             foreach ($body["items"] as $track) {
                 $topTrackId = $track["id"];
-                array_push($seeds["tracks"], $topTrackId);
+                array_push($this->seeds["tracks"], $topTrackId);
+            }
+        }
+    }
+
+    private function getUserSavedTracks()
+    {
+        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/me/tracks?limit=5", [], ["Authorization" => "Bearer " . $this->access_token]);
+        $body = json_decode($response["body"], true);
+        $status = $response["status"];
+
+        if ($status >= 300) {
+            Renderer::getInstance()->internalError();
+            die;
+        }
+
+        if (count($body["items"]) > 0) {
+            foreach ($body["items"] as $item) {
+                $savedTrackId = $item["track"]["id"];
+                array_push($this->seeds["tracks"], $savedTrackId);
+            }
+        }
+    }
+
+
+    private function validSeeds()
+    {
+        return count($this->seeds["artists"]) > 0 || count($this->seeds["tracks"]) > 0 || count($this->seeds["genres"]) > 0;
+    }
+
+    private function getRecommendations()
+    {
+        $artists = "";
+        $genres = "";
+        $tracks = count($this->seeds["tracks"]) > 0 ? $this->seeds["tracks"][0] : "";
+
+        foreach ($this->seeds["artists"] as $artistId) {
+            if (strlen($artists) > 0) {
+                $artists .= "%2C" . $artistId;
+            } else {
+                $artists .= $artistId;
             }
         }
 
-        $seeds = [
-            "tracks" => [],
-            "artists" => [],
-            "genres" => []
-        ];
-        //? Valida si se pudo personalizar la recomendación, sino recomienda ultimos lanzamientos (info. más genérica).
-        if (count($seeds["artists"]) > 0 || count($seeds["tracks"]) > 0) {
-            //? Obtiene recomendaciones personalizadas para el usuario
-            $artists = "";
-            $genres = "";
-            $tracks = count($seeds["tracks"]) > 0 ? $seeds["tracks"][0] : "";
+        foreach ($this->seeds["genres"] as $genre) {
+            $words = explode(" ", $genre);
+            $genre = "";
 
-            foreach ($seeds["artists"] as $artistId) {
-                if (strlen($artists) > 0) {
-                    $artists .= "%2C" . $artistId;
+            foreach ($words as $word) {
+                if (strlen($genre) > 0) {
+                    $genre .= "+" . $word;
                 } else {
-                    $artists .= $artistId;
+                    $genre .= $word;
                 }
             }
 
-            foreach ($seeds["genres"] as $genre) {
-                $words = explode(" ", $genre);
-                $genre = "";
-
-                foreach ($words as $word) {
-                    if (strlen($genre) > 0) {
-                        $genre .= "+" . $word;
-                    } else {
-                        $genre .= $word;
-                    }
-                }
-
-                if (strlen($genres) > 0) {
-                    $genres .= "%2C" . $genre;
-                } else {
-                    $genres .= $genre;
-                }
+            if (strlen($genres) > 0) {
+                $genres .= "%2C" . $genre;
+            } else {
+                $genres .= $genre;
             }
-
-            $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/recommendations?seed_artists=$artists&seed_tracks=$tracks&seed_genres=$genres&limit=10", [], ["Authorization" => "Bearer " . $access_token]);
-            $body = json_decode($response["body"], true);
-            $status = $response["status"];
-
-            if ($status >= 300) {
-                Renderer::getInstance()->internalError();
-                die;
-            }
-
-            // echo "<pre>";
-            // var_dump($body["tracks"]);
-            // die;
-        } else {
-            //? Recomendamos nuevos lanzamientos al usuario
-            $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/browse/new-releases", ["limit", 10], ["Authorization" => "Bearer " . $access_token]);
-            $body = json_decode($response["body"], true);
-            $status = $response["status"];
-            // echo "<pre>";
-            // var_dump($body["albums"]);
-            // die;
         }
-        Renderer::getInstance()->explore();
+
+        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/recommendations?seed_artists=$artists&seed_tracks=$tracks&seed_genres=$genres&limit=10", [], ["Authorization" => "Bearer " . $this->access_token]);
+        $body = json_decode($response["body"], true);
+        $status = $response["status"];
+
+        if ($status >= 300) {
+            Renderer::getInstance()->internalError();
+            die;
+        }
+
+        //TODO: Retornar solo info necesaria de los tracks
+        return $body["tracks"];
+    }
+
+    private function getNewReleases()
+    {
+        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/browse/new-releases", ["limit", 10], ["Authorization" => "Bearer " . $this->access_token]);
+        $body = json_decode($response["body"], true);
+        $status = $response["status"];
+
+        if ($status >= 300) {
+            Renderer::getInstance()->internalError();
+            die;
+        }
+
+        //TODO: Retornar solo info necesaria de los albums
+        return $body["albums"];
     }
 }
