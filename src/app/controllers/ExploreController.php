28754f6689f7926decd3e622de309c670f2e93ae
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-06-24 12:09:11 -0300

    feat: agrega petición batch de imagenes de artistas

--- src/app/controllers/ExploreController.php
+++ src/app/controllers/ExploreController.php
@@ -45,11 +45,11 @@ class ExploreController extends Controller
         }
         
 
-        // echo "<pre>";
-        // var_dump($userTopTracks);
-        // var_dump($newReleases);
-        // var_dump($recommendations);
-        // die;
+        echo "<pre>";
+        var_dump($userTopTracks);
+        var_dump($newReleases);
+        var_dump($recommendations);
+        die;
 
         //TODO: Renderizar explore.view.php
         Renderer::getInstance()->explore();
@@ -91,6 +91,8 @@ class ExploreController extends Controller
 
         $userTopTracks = [];
 
+        $ids = "";
+
         if (count($body["items"]) > 0) {
             foreach ($body["items"] as $track) {
                 $topTrackId = $track["id"];
@@ -100,8 +102,8 @@ class ExploreController extends Controller
                     "type" => $track["album"]["album_type"],
                     "artist_name" => $track["album"]["artists"][0]["name"],
                     "artist_id" => $track["album"]["artists"][0]["id"],
-                    "artist_spotify_url" => $track["album"]["external_urls"]["spotify"],
-                    "artist_api_url" => $track["album"]["href"],
+                    "artist_spotify_url" => $track["album"]["artists"][0]["external_urls"]["spotify"],
+                    "artist_api_url" => $track["album"]["artists"][0]["href"],
                     "album_spotify_url" => $track["album"]["external_urls"]["spotify"],
                     "album_api_url" => $track["album"]["href"],
                     "images" => $track["album"]["images"],
@@ -114,10 +116,31 @@ class ExploreController extends Controller
                     "track_preview_url" => $track["preview_url"],
                 ];
 
+                if(strlen($ids) > 0 ){
+                    $ids .= "," . $track["artist_id"];
+                } else {
+                    $ids .= $track["artist_id"];
+                }
+
                 array_push($userTopTracks, $track);
+                
             }
         }
-
+        
+        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/artists", ["ids" => $ids], ["Authorization" => "Bearer " . $this->access_token]);
+        $body = json_decode($response["body"], true);
+        $status = $response["status"];
+        
+        if ($status >= 300) {
+            Renderer::getInstance()->internalError();
+            die;
+        }
+        
+        for($i = 0; $i < count($body["artists"]); $i++){
+            $userTopTracks[$i]["artist_avatar_url"] = $body["artists"][$i]["images"][1];
+        }
+        
+        // TODO: Save data to DB
         return $userTopTracks;
     }
 
@@ -200,6 +223,7 @@ class ExploreController extends Controller
         }
 
         $recommendations = [];
+        $ids = "";
 
         if (count($body["tracks"]) > 0) {
             foreach ($body["tracks"] as $track) {
@@ -208,8 +232,8 @@ class ExploreController extends Controller
                     "type" => $track["album"]["album_type"],
                     "artist_name" => $track["album"]["artists"][0]["name"],
                     "artist_id" => $track["album"]["artists"][0]["id"],
-                    "artist_spotify_url" => $track["album"]["external_urls"]["spotify"],
-                    "artist_api_url" => $track["album"]["href"],
+                    "artist_spotify_url" => $track["album"]["artists"][0]["external_urls"]["spotify"],
+                    "artist_api_url" => $track["album"]["artists"][0]["href"],
                     "album_spotify_url" => $track["album"]["external_urls"]["spotify"],
                     "album_api_url" => $track["album"]["href"],
                     "images" => $track["album"]["images"],
@@ -222,16 +246,36 @@ class ExploreController extends Controller
                     "track_preview_url" => $track["preview_url"],
                 ];
 
+                if(strlen($ids) > 0 ){
+                    $ids .= "," . $track["artist_id"];
+                } else {
+                    $ids .= $track["artist_id"];
+                }
+
                 array_push($recommendations, $track);
             }
         }
-
+        
+        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/artists", ["ids" => $ids], ["Authorization" => "Bearer " . $this->access_token]);
+        $body = json_decode($response["body"], true);
+        $status = $response["status"];
+        
+        if ($status >= 300) {
+            Renderer::getInstance()->internalError();
+            die;
+        }
+        
+        for($i = 0; $i < count($body["artists"]); $i++){
+            $recommendations[$i]["artist_avatar_url"] = $body["artists"][$i]["images"][1];
+        }
+        
+        // TODO: Save data to DB
         return $recommendations;
     }
 
     private function getNewReleases()
     {
-        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/browse/new-releases", ["limit", 10], ["Authorization" => "Bearer " . $this->access_token]);
+        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/browse/new-releases", ["limit" => 10], ["Authorization" => "Bearer " . $this->access_token]);
         $body = json_decode($response["body"], true);
         $status = $response["status"];
 
@@ -241,6 +285,7 @@ class ExploreController extends Controller
         }
 
         $newReleases = [];
+        $ids = "";
 
         if (count($body["albums"]["items"]) > 0) {
             foreach ($body["albums"]["items"] as $item) {
@@ -258,10 +303,30 @@ class ExploreController extends Controller
                     "release_date" => $item["release_date"],
                 ];
 
+                if(strlen($ids) > 0 ){
+                    $ids .= "," . $album["artist_id"];
+                } else {
+                    $ids .= $album["artist_id"];
+                }
+
                 array_push($newReleases, $album);
             }
         }
 
+        $response = HttpClient::getInstance()->get("https://api.spotify.com/v1/artists", ["ids" => $ids], ["Authorization" => "Bearer " . $this->access_token]);
+        $body = json_decode($response["body"], true);
+        $status = $response["status"];
+        
+        if ($status >= 300) {
+            Renderer::getInstance()->internalError();
+            die;
+        }
+        
+        for($i = 0; $i < count($body["artists"]); $i++){
+            $newReleases[$i]["artist_avatar_url"] = $body["artists"][$i]["images"][1];
+        }
+
+        // TODO: Save data to DB
         return $newReleases;
     }
 }
