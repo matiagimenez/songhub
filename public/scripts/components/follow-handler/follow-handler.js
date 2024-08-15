b0babd2beaeac6b0b7cd92a8ae279c3cdc90ddbb
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-08-15 10:39:23 -0300

    fix: actualiza contador de seguidores del usuario cuando se da click a segui

--- public/scripts/components/follow-handler/follow-handler.js
+++ public/scripts/components/follow-handler/follow-handler.js
@@ -1,31 +1,48 @@
-document.addEventListener('click', (event) => {
+const followersCount = document.querySelector('#followers-count');
 
-    if (!event.target.classList.contains('action-button')) return;
-    
+document.addEventListener('click', (event) => {
+	if (!event.target.classList.contains('action-button')) return;
+
 	const currentButton = event.target;
-    
-	if (currentButton.classList.contains('submit-button')) {
-        
-        fetch('/follow/user?user='+currentButton.id)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('Success:', data);
-        })
-        .catch((error) => console.error('Error:', error));
+
+	if (currentButton.classList.contains('follow')) {
+		fetch('/follow/user?user=' + currentButton.id)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				if (data.success) {
+					followersCount.innerText =
+						parseInt(followersCount.textContent) + 1;
+					currentButton.classList.remove('follow');
+					currentButton.classList.add('unfollow');
+					currentButton.classList.remove('submit-button');
+					currentButton.classList.add('submit-outline-button');
+				}
+			})
+			.catch((error) => console.error('Error:', error));
 	}
 
-	if (currentButton.classList.contains('cancel-outline-button')) {
-		fetch('/unfollow/user?user='+currentButton.id, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-        },
-            body: JSON.stringify({ user: currentButton.id })
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('Success:', data);
-        })
-        .catch((error) => console.error('Error:', error));
+	if (currentButton.classList.contains('unfollow')) {
+		fetch('/unfollow/user?user=' + currentButton.id, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ user: currentButton.id }),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				if (data.success) {
+					followersCount.innerText =
+						parseInt(followersCount.textContent) - 1;
+					currentButton.classList.remove('unfollow');
+					currentButton.classList.add('follow');
+					currentButton.classList.add('submit-button');
+					currentButton.innerText = 'Seguir';
+					currentButton.classList.remove('submit-outline-button');
+				}
+			})
+			.catch((error) => console.error('Error:', error));
 	}
 });
