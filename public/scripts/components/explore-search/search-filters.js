4200c0920ca2dbea72ce8e6348611d30b1a7ce4b
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-08-17 18:34:20 -0300

    feat: cambia el valor del label en la busqueda dependiendo del contenido deseado

--- public/scripts/components/explore-search/search-filters.js
+++ public/scripts/components/explore-search/search-filters.js
@@ -1,24 +1,27 @@
 const contentButton = document.getElementById('content-button');
 const profilesButton = document.getElementById('profiles-button');
+const searchDescriptionLabel = document.getElementById('search-description');
 
 let isActiveContent = true;
 
 function setContentFilter() {
-  contentButton.classList.remove('inactive');
-  contentButton.classList.add('active');
-  profilesButton.classList.remove('active');
-  profilesButton.classList.add('inactive');
-  window.isActiveContent = true;
-  window.searchManagement();
+	searchDescriptionLabel.innerText = 'Buscar álbum, canción o artista';
+	contentButton.classList.remove('inactive');
+	contentButton.classList.add('active');
+	profilesButton.classList.remove('active');
+	profilesButton.classList.add('inactive');
+	window.isActiveContent = true;
+	window.searchManagement();
 }
 
 function setProfileFilter() {
-  contentButton.classList.remove('active');
-  contentButton.classList.add('inactive');
-  profilesButton.classList.remove('inactive');
-  profilesButton.classList.add('active');
-  window.isActiveContent = false;
-  window.searchManagement();
+	searchDescriptionLabel.innerText = 'Buscar usuarios';
+	contentButton.classList.remove('active');
+	contentButton.classList.add('inactive');
+	profilesButton.classList.remove('inactive');
+	profilesButton.classList.add('active');
+	window.isActiveContent = false;
+	window.searchManagement();
 }
 
 contentButton.addEventListener('click', setContentFilter);
