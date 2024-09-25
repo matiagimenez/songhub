7cc59f34318a8a6479b9ad81857b7f0a8839b37f
SimonDiLeoGIT <saimondiley@gmail.com>
2024-09-25 11:18:10 -0300

    fix: fix explore height styles en .add modal access modo celular

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,302 +1,300 @@
 export const routes = {
-	'/login': [
-		{
-			scriptName: 'input-animation',
-			scriptPath: '/scripts/animations/input-animation.js',
-		},
-	],
-	'/register': [
-		{
-			scriptName: 'input-animation',
-			scriptPath: '/scripts/animations/input-animation.js',
-		},
-	],
-	'/explore': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
+  "/login": [
+    {
+      scriptName: "input-animation",
+      scriptPath: "/scripts/animations/input-animation.js",
+    },
+  ],
+  "/register": [
+    {
+      scriptName: "input-animation",
+      scriptPath: "/scripts/animations/input-animation.js",
+    },
+  ],
+  "/explore": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
 
-		{
-			scriptName: 'modal-form',
-			scriptPath: '/scripts/components/modal-form/modal-form.js',
-		},
-		{
-			scriptName: 'favorite-button',
-			scriptPath:
-				'/scripts/components/favorite-button/favorite-button.js',
-		},
-		{
-			scriptName: 'searcher',
-			scriptPath: '/scripts/components/searcher/searcher.js',
-		},
-		{
-			scriptName: 'pagination',
-			scriptPath: '/scripts/components/pagination/pagination.js',
-		},
-		{
-			scriptName: 'logout-button',
-			scriptPath: '/scripts/components/logout-button/logout-button.js',
-		},
-		{
-			scriptName: 'footer-animation',
-			scriptPath: '/scripts/animations/footer-animation.js',
-		},
-		{
-			scriptName: 'search-filters',
-			scriptPath: '/scripts/components/explore-search/search-filters.js',
-		},
-		{
-			scriptName: 'blur-image-load',
-			scriptPath: '/scripts/animations/blur-image-load.js',
-		},
-	],
-	'/user/followers': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'action-button-animation',
-			scriptPath: '/scripts/animations/action-button-animation.js',
-		},
-		{
-			scriptName: 'logout-button',
-			scriptPath: '/scripts/components/logout-button/logout-button.js',
-		},
-		{
-			scriptName: 'follow-handler',
-			scriptPath: '/scripts/components/follow-handler/follow-handler.js',
-		},
-		{
-			scriptName: 'follow-list',
-			scriptPath: '/scripts/components/follow-list/follow-list.js',
-		},
-	],
-	'/user/following': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'action-button-animation',
-			scriptPath: '/scripts/animations/action-button-animation.js',
-		},
-		{
-			scriptName: 'logout-button',
-			scriptPath: '/scripts/components/logout-button/logout-button.js',
-		},
-		{
-			scriptName: 'follow-handler',
-			scriptPath: '/scripts/components/follow-handler/follow-handler.js',
-		},
-		{
-			scriptName: 'follow-list',
-			scriptPath: '/scripts/components/follow-list/follow-list.js',
-		},
-	],
-	'/post': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'likes-counter-animation',
-			scriptPath: '/scripts/animations/likes-counter-animation.js',
-		},
-		{
-			scriptName: 'likes-handler',
-			scriptPath: '/scripts/components/likes-handler/likes-handler.js',
-		},
-		{
-			scriptName: 'logout-button',
-			scriptPath: '/scripts/components/logout-button/logout-button.js',
-		},
-		{
-			scriptName: 'footer-animation',
-			scriptPath: '/scripts/animations/footer-animation.js',
-		},
-	],
+    {
+      scriptName: "modal-form",
+      scriptPath: "/scripts/components/modal-form/modal-form.js",
+    },
+    {
+      scriptName: "favorite-button",
+      scriptPath: "/scripts/components/favorite-button/favorite-button.js",
+    },
+    {
+      scriptName: "searcher",
+      scriptPath: "/scripts/components/searcher/searcher.js",
+    },
+    {
+      scriptName: "pagination",
+      scriptPath: "/scripts/components/pagination/pagination.js",
+    },
+    {
+      scriptName: "logout-button",
+      scriptPath: "/scripts/components/logout-button/logout-button.js",
+    },
+    {
+      scriptName: "footer-animation",
+      scriptPath: "/scripts/animations/footer-animation.js",
+    },
+    {
+      scriptName: "search-filters",
+      scriptPath: "/scripts/components/explore-search/search-filters.js",
+    },
+    {
+      scriptName: "blur-image-load",
+      scriptPath: "/scripts/animations/blur-image-load.js",
+    },
+  ],
+  "/user/followers": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "action-button-animation",
+      scriptPath: "/scripts/animations/action-button-animation.js",
+    },
+    {
+      scriptName: "logout-button",
+      scriptPath: "/scripts/components/logout-button/logout-button.js",
+    },
+    {
+      scriptName: "follow-handler",
+      scriptPath: "/scripts/components/follow-handler/follow-handler.js",
+    },
+    {
+      scriptName: "follow-list",
+      scriptPath: "/scripts/components/follow-list/follow-list.js",
+    },
+  ],
+  "/user/following": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "action-button-animation",
+      scriptPath: "/scripts/animations/action-button-animation.js",
+    },
+    {
+      scriptName: "logout-button",
+      scriptPath: "/scripts/components/logout-button/logout-button.js",
+    },
+    {
+      scriptName: "follow-handler",
+      scriptPath: "/scripts/components/follow-handler/follow-handler.js",
+    },
+    {
+      scriptName: "follow-list",
+      scriptPath: "/scripts/components/follow-list/follow-list.js",
+    },
+  ],
+  "/post": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "likes-counter-animation",
+      scriptPath: "/scripts/animations/likes-counter-animation.js",
+    },
+    {
+      scriptName: "likes-handler",
+      scriptPath: "/scripts/components/likes-handler/likes-handler.js",
+    },
+    {
+      scriptName: "logout-button",
+      scriptPath: "/scripts/components/logout-button/logout-button.js",
+    },
+    {
+      scriptName: "footer-animation",
+      scriptPath: "/scripts/animations/footer-animation.js",
+    },
+  ],
 
-	'/user': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'likes-counter-animation',
-			scriptPath: '/scripts/animations/likes-counter-animation.js',
-		},
-		{
-			scriptName: 'logout-button',
-			scriptPath: '/scripts/components/logout-button/logout-button.js',
-		},
-		{
-			scriptName: 'follow-handler',
-			scriptPath: '/scripts/components/follow-handler/follow-handler.js',
-		},
-		{
-			scriptName: 'action-button-animation',
-			scriptPath: '/scripts/animations/action-button-animation.js',
-		},
-		{
-			scriptName: 'feed',
-			scriptPath: '/scripts/components/feed/feed.js',
-		},
-		{
-			scriptName: 'likes-handler',
-			scriptPath: '/scripts/components/likes-handler/likes-handler.js',
-		},
-	],
-	'/search': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'modal-form',
-			scriptPath: '/scripts/components/modal-form/modal-form.js',
-		},
-		{
-			scriptName: 'logout-button',
-			scriptPath: '/scripts/components/logout-button/logout-button.js',
-		},
-		{
-			scriptName: 'search-content',
-			scriptPath: '/scripts/components/search-content/search-content.js',
-		},
-		{
-			scriptName: 'pagination',
-			scriptPath: '/scripts/components/pagination/pagination.js',
-		},
-	],
-	'/content': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'favorite-button',
-			scriptPath:
-				'/scripts/components/favorite-button/favorite-button.js',
-		},
-		{
-			scriptName: 'likes-counter-animation',
-			scriptPath: '/scripts/animations/likes-counter-animation.js',
-		},
-		{
-			scriptName: 'logout-button',
-			scriptPath: '/scripts/components/logout-button/logout-button.js',
-		},
-		{
-			scriptName: 'track-preview-button',
-			scriptPath:
-				'/scripts/components/track-preview-button/track-preview-button.js',
-		},
-		{
-			scriptName: 'footer-animation',
-			scriptPath: '/scripts/animations/footer-animation.js',
-		},
-		{
-			scriptName: 'modal-form',
-			scriptPath: '/scripts/components/modal-form/modal-form.js',
-		},
-	],
-	'/user/profile': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'logout-button',
-			scriptPath: '/scripts/components/logout-button/logout-button.js',
-		},
-		{
-			scriptName: 'favorite-button',
-			scriptPath:
-				'/scripts/components/favorite-button/remove-favorite-button.js',
-		},
-	],
-	'/terms-conditions': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'logout-button',
-			scriptPath: '/scripts/components/logout-button/logout-button.js',
-		},
-		{
-			scriptName: 'footer-animation',
-			scriptPath: '/scripts/animations/footer-animation.js',
-		},
-	],
-	'/error': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'logout-button',
-			scriptPath: '/scripts/components/logout-button/logout-button.js',
-		},
-		{
-			scriptName: 'footer-animation',
-			scriptPath: '/scripts/animations/footer-animation.js',
-		},
-	],
-	'/user/password-recovery': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'footer-animation',
-			scriptPath: '/scripts/animations/footer-animation.js',
-		},
-		{
-			scriptName: 'password-animation',
-			scriptPath: '/scripts/animations/password-animation.js',
-		},
-	],
-	'/edit-password': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'footer-animation',
-			scriptPath: '/scripts/animations/footer-animation.js',
-		},
-		{
-			scriptName: 'password-animation',
-			scriptPath: '/scripts/animations/password-animation.js',
-		},
-	],
-	'/': [
-		{
-			scriptName: 'menu-animation',
-			scriptPath: '/scripts/animations/menu-animation.js',
-		},
-		{
-			scriptName: 'expanding-cards',
-			scriptPath: '/scripts/components/carousel/carousel.js',
-		},
-		{
-			scriptName: 'feed',
-			scriptPath: '/scripts/components/feed/feed.js',
-		},
-		{
-			scriptName: 'likes-counter-animation',
-			scriptPath: '/scripts/animations/likes-counter-animation.js',
-		},
-		{
-			scriptName: 'logout-button',
-			scriptPath: '/scripts/components/logout-button/logout-button.js',
-		},
-		{
-			scriptName: 'likes-handler',
-			scriptPath: '/scripts/components/likes-handler/likes-handler.js',
-		},
-	],
+  "/user": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "likes-counter-animation",
+      scriptPath: "/scripts/animations/likes-counter-animation.js",
+    },
+    {
+      scriptName: "logout-button",
+      scriptPath: "/scripts/components/logout-button/logout-button.js",
+    },
+    {
+      scriptName: "follow-handler",
+      scriptPath: "/scripts/components/follow-handler/follow-handler.js",
+    },
+    {
+      scriptName: "action-button-animation",
+      scriptPath: "/scripts/animations/action-button-animation.js",
+    },
+    {
+      scriptName: "feed",
+      scriptPath: "/scripts/components/feed/feed.js",
+    },
+    {
+      scriptName: "likes-handler",
+      scriptPath: "/scripts/components/likes-handler/likes-handler.js",
+    },
+  ],
+  "/search": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "modal-form",
+      scriptPath: "/scripts/components/modal-form/modal-form.js",
+    },
+    {
+      scriptName: "logout-button",
+      scriptPath: "/scripts/components/logout-button/logout-button.js",
+    },
+    {
+      scriptName: "search-content",
+      scriptPath: "/scripts/components/search-content/search-content.js",
+    },
+    {
+      scriptName: "pagination",
+      scriptPath: "/scripts/components/pagination/pagination.js",
+    },
+  ],
+  "/content": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "favorite-button",
+      scriptPath: "/scripts/components/favorite-button/favorite-button.js",
+    },
+    {
+      scriptName: "likes-counter-animation",
+      scriptPath: "/scripts/animations/likes-counter-animation.js",
+    },
+    {
+      scriptName: "logout-button",
+      scriptPath: "/scripts/components/logout-button/logout-button.js",
+    },
+    {
+      scriptName: "track-preview-button",
+      scriptPath:
+        "/scripts/components/track-preview-button/track-preview-button.js",
+    },
+    {
+      scriptName: "footer-animation",
+      scriptPath: "/scripts/animations/footer-animation.js",
+    },
+    {
+      scriptName: "modal-form",
+      scriptPath: "/scripts/components/modal-form/modal-form.js",
+    },
+  ],
+  "/user/profile": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "logout-button",
+      scriptPath: "/scripts/components/logout-button/logout-button.js",
+    },
+    {
+      scriptName: "favorite-button",
+      scriptPath:
+        "/scripts/components/favorite-button/remove-favorite-button.js",
+    },
+  ],
+  "/terms-conditions": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "logout-button",
+      scriptPath: "/scripts/components/logout-button/logout-button.js",
+    },
+    {
+      scriptName: "footer-animation",
+      scriptPath: "/scripts/animations/footer-animation.js",
+    },
+  ],
+  "/error": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "logout-button",
+      scriptPath: "/scripts/components/logout-button/logout-button.js",
+    },
+    {
+      scriptName: "footer-animation",
+      scriptPath: "/scripts/animations/footer-animation.js",
+    },
+  ],
+  "/user/password-recovery": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "footer-animation",
+      scriptPath: "/scripts/animations/footer-animation.js",
+    },
+    {
+      scriptName: "password-animation",
+      scriptPath: "/scripts/animations/password-animation.js",
+    },
+  ],
+  "/edit-password": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "footer-animation",
+      scriptPath: "/scripts/animations/footer-animation.js",
+    },
+    {
+      scriptName: "password-animation",
+      scriptPath: "/scripts/animations/password-animation.js",
+    },
+  ],
+  "/": [
+    {
+      scriptName: "menu-animation",
+      scriptPath: "/scripts/animations/menu-animation.js",
+    },
+    {
+      scriptName: "expanding-cards",
+      scriptPath: "/scripts/components/carousel/carousel.js",
+    },
+    {
+      scriptName: "feed",
+      scriptPath: "/scripts/components/feed/feed.js",
+    },
+    {
+      scriptName: "likes-counter-animation",
+      scriptPath: "/scripts/animations/likes-counter-animation.js",
+    },
+    {
+      scriptName: "logout-button",
+      scriptPath: "/scripts/components/logout-button/logout-button.js",
+    },
+    {
+      scriptName: "likes-handler",
+      scriptPath: "/scripts/components/likes-handler/likes-handler.js",
+    },
+  ],
 };
